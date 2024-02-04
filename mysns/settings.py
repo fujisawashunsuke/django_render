2d68b240defa82d2852cf345c5804956bd2e489f
fujisawashunsuke <shunsuke200752@icloud.com>
2024-02-04 09:25:46 +0900

    render用ファイル追加

--- mysns/settings.py
+++ mysns/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT=str(os.path.join(BASE_DIR,"staticfiles"))
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-]
+
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
